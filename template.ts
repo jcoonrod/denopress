91e10c9ccab432c45c23ac411c7326efc7bb3d48
John Coonrod <john.coonrod@thp.org>
2021-07-27 09:55:31 -0400

    add wp-blocks

--- template.ts
+++ template.ts
@@ -3,13 +3,14 @@ export function template(site: string, menu: string, content: string){
 <html lang=en>
     <head>
         <title>${site}</title>
+        <link rel=stylesheet type="text/css" href=/static/block-libary.css>
         <link rel=stylesheet type="text/css" href=/static/style.css>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0">
         <meta name="description" content="Re-creating WordPress in JavaScript and Deno.">
     </head>
     <body>
-        <a href=/><h1>${site}</h1></a>
+        <a href=/><h1>${site} </h1></a>
         <nav id="menu-primary" role="navigation"><ul>${menu}</ul></nav>
         ${content}
     </body>
